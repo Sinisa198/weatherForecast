4f3602b031735f261535dc5453571b142e0e5214
SinisaLjubanovic <sinisaljubanovic76@gmail.com>
2022-06-27 10:36:56 +0200

    Install axios and bootstrap

--- weather-forecast/src/App.tsx
+++ weather-forecast/src/App.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import SearchInput from "./components/SearchInput";
 function App() {
   return (
     <div className="App">
       <h1>Hello!</h1>
+      <SearchInput />
     </div>
   );
 }
---
+++ weather-forecast/src/components/SearchInput.tsx
@@ -0,0 +1,11 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const SearchInput = () => {
+  return (
+    <div>
+      <input type="serch" placeholder="Enter Location" />
+    </div>
+  );
+};
+
+export default SearchInput;
